528a6a526c340b23f0ad8f37563a1c20e8d354b8
Lillianmon <xli202@jhmi.edu>
2020-07-02 11:09:26 -0400

    Examples updates for scoping rules

--- Scoping rules.R
+++ Scoping rules.R
@@ -11,3 +11,17 @@ f <- function(x, y){
 #free variables are not formal arguments and are not local variables
 
 #lexical scoping in R means: the values of free variables are searched for in the environment in which the function was defined
+
+
+#Example of Lexical scoping
+y <- 10
+f <- function(x){
+  y <- 2
+  y^2 + g(x)
+}
+g <- function(x){
+  x*y
+}
+
+#f(3) = 130, y = 10, the value in the function g is looked up in the environment in which the function was defined (the global environment)
+#However, in other languages they use dynamic scoping, in which the value of y is looked up in the environment from which the function was called, and y=2
